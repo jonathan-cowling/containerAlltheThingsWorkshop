c8ed8f8360c52ca20baef741b11bb851c3703537
Jonathan Cowling <jonathan.cowling@moneysupermarket.com>
2019-06-17 17:42:50 +0100

    Step 4: get servers talking to one another
    test with:
    `while true; do echo $(curl -s localhost:30000); sleep 0.1; done`

--- server1/src/main/kotlin/tk/jonathancowling/server1/Controller.kt
+++ server1/src/main/kotlin/tk/jonathancowling/server1/Controller.kt
@@ -3,6 +3,8 @@ package tk.jonathancowling.server1
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
 import java.net.InetAddress
 
 @RestController
@@ -10,6 +12,12 @@ class Controller {
 
     @RequestMapping(path = [ "/" ], method = [ RequestMethod.GET ])
     fun home(): String {
-        return "hello from server1 at ${InetAddress.getLocalHost().hostAddress}"
+        val res = RestTemplate().getForEntity<String>("http://server2.default.svc.cluster.local:8080/address")
+        return "hello from server1 at ${InetAddress.getLocalHost().hostAddress}, spoke to server2 at ${res.body}"
+    }
+
+    @RequestMapping(path = [ "/address" ], method = [ RequestMethod.GET ])
+    fun address(): String {
+        return InetAddress.getLocalHost().hostAddress
     }
 }
--- server2/src/main/kotlin/tk/jonathancowling/server2/Controller.kt
+++ server2/src/main/kotlin/tk/jonathancowling/server2/Controller.kt
@@ -3,6 +3,8 @@ package tk.jonathancowling.server2
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
 import java.net.InetAddress
 
 @RestController
@@ -10,6 +12,12 @@ class Controller {
 
     @RequestMapping(path = [ "/" ], method = [ RequestMethod.GET ])
     fun home(): String {
-        return "hello from server2 at ${InetAddress.getLocalHost().hostAddress}"
+        val res = RestTemplate().getForEntity<String>("http://server1.default.svc.cluster.local:8080/address")
+        return "hello from server2 at ${InetAddress.getLocalHost().hostAddress}, spoke to server1 at ${res.body}"
+    }
+
+    @RequestMapping(path = [ "/address" ], method = [ RequestMethod.GET ])
+    fun address(): String {
+        return InetAddress.getLocalHost().hostAddress
     }
 }
